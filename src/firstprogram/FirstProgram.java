53fbef4058118b22418e09214e84e1b0d5c0bdae
anton <anton@pc-itteacher.cbcstjohns.co.za>
2016-11-29 09:27:02 +0200

    change1

--- src/firstprogram/FirstProgram.java
+++ src/firstprogram/FirstProgram.java
@@ -15,7 +15,7 @@ public class FirstProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new FirstProgramForm().setVisible(true);
     }
     
 }
